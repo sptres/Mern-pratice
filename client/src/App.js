ba2786e606fcca2377910193673185ea6fec0208
unknown <shawnpark2397@gmail.com>
2023-09-07 22:45:58 -0700

    .

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Hike Seek Supplies</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
